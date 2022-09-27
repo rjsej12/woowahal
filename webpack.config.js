ba6726a1d3d1cb225b9fe9eebc88711006e29263
PisoDev77 <pisodev0427@gmail.com>
2022-09-27 11:25:26 +0900

    webpack JS 경로 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 module.exports={
-    entry: "./src/js/app.js",
-    output: {
-        path: __dirname+"/public/",
-        filename: "bundle.js",
-      },
-      watch: true,
-      mode: "development"
+  entry: {
+    cart: './src/js/pages/cart.js',
+    category: './src/js/pages/category.js',
+    main: './src/js/pages/main.js',
+    result: './src/js/pages/result.js',
+    store_detail: './src/js/pages/store_detail.js',
+    store_list: './src/js/pages/store_list.js',
+    store_order: './src/js/pages/store_order.js',
+  },
+  output: {
+    path: __dirname+"/public/js/",
+    filename: "[name].js",
+  },
+
+  watch: true,
+  mode: "development"
 }
