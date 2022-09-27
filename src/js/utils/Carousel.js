a5d1041213012af4ef9697c9507cb2cc68759682
PisoDev77 <pisodev0427@gmail.com>
2022-09-27 16:39:48 +0900

    자바스크립트 파일들 추가 및 생성

---
+++ src/js/utils/Carousel.js
@@ -0,0 +1,78 @@
+import {addClassList, toggleClassList} from "./ClassListHandler.js";
+import carouselUrl from "../mock/carousel.json";
+
+const makeSlide = (slider, item, idx) => {
+    
+    const slide = document.createElement("li");
+    const slideAncher = document.createElement("a");
+        const slideTitle = document.createElement("h3");
+        const slideContent = document.createElement("p");
+
+        slideAncher.appendChild(slideTitle);
+        slideAncher.appendChild(slideContent);
+        slideTitle.innerText = item.title;
+        slideContent.innerText = item.content;
+        slideAncher.style.backgroundImage = `url(${item.url})`;
+        slideAncher.href="#";
+
+    slide.appendChild(slideAncher);
+
+    addClassList(slide, "carousel-slide");
+    if(idx === 0 ) {addClassList(slide, "is-active");}
+    
+    slider.appendChild(slide);
+}
+const makeOffscreen = (carousel, urls) => {
+    const offscreen = carousel.querySelector(".offscreen-text");
+    offscreen.innerText = `1 / ${urls.length}`;
+}
+const toggleActive = (curr, comp, str) => {
+    if(comp.tagName === 'LI'){
+        toggleClassList(curr, str);
+        toggleClassList(comp, str);
+        comp.querySelector("a").focus();
+    }
+}
+
+const Carousel = (carousels) => {
+
+    for(let carousel of carousels){
+        const slider = carousel.querySelector(".carousel-slider");
+        if(carousel.classList.contains("promotion")){
+
+            const promotion = carouselUrl.promotion;
+            promotion.map((item,idx)=>{
+                makeSlide(slider, item, idx);
+            });
+
+            makeOffscreen(carousel, promotion);
+        }
+        if(carousel.classList.contains("advertisement")){
+            const advertisement = carouselUrl.advertisement;
+            advertisement.map((item,idx)=>{
+                makeSlide(slider, item, idx);
+            });
+
+            makeOffscreen(carousel, advertisement);
+        }
+
+        carousel.addEventListener("keydown",(e)=>{
+            const keyCode = e.keyCode;
+            const currentActive = carousel.querySelector(".is-active");
+            //right
+            if(keyCode === 39){
+                const nextActive = currentActive.nextSibling;
+                toggleActive(currentActive,nextActive,"is-active");
+            }
+            // LEFT
+            if(keyCode === 37){
+                const prevActive = currentActive.previousSibling;
+                toggleActive(currentActive,prevActive,"is-active");
+            }
+
+           
+        });
+    }
+}
+
+export default Carousel
