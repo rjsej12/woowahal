b858da820eabe5420b734e28d8c5dc71b07a3c27
PisoDev77 <pisodev0427@gmail.com>
2022-09-28 17:07:18 +0900

    디테일 동적 리스트 구현 완성

--- src/js/pages/store_detail/index.js
+++ src/js/pages/store_detail/index.js
@@ -1,8 +1,8 @@
 import {toggleClassList} from "../../utils/ClassListHandler.js";
-import { getParam, getStore } from "../../utils/Param.js";
+import { getParam, getStore, getMenus } from "../../utils/Param.js";
+import { convertPrices } from "../../utils/Wrapping.js";
 
-const menu_btns = document.querySelectorAll(".menu-btn");
-const cart = document.querySelector(".st-cart");
+const menus_list = document.querySelector(".st-detail-menus");
 
 
 const params = getParam(location.search);
@@ -26,14 +26,51 @@ const setDetail = () => {
         `;
         rating.querySelector(".colorBox").style.setProperty("clip-path",`polygon(0 0, ${store.rating}% 0, ${store.rating}% 100%, 0% 100%)`);
         
-        console.log(`polygon(0 0, ${store.rating}% 0, ${store.rating}% 100%, 0% 100%);`);
 }
 
+const setMenus = () => {
+    const menus = getMenus(params.id);
+    const categorys = [];
+    let el = ``;
+    menus.map((menu)=>{
+        if(!categorys.includes(menu.category)){
+            categorys.push(menu.category);
+            if(el !== ``){
+                el += "</ul>"
+            }
+            el += `
+                <button class="menu-btn">${menu.category}<i class="fa-sharp fa-solid fa-chevron-up"></i></button>
+                <ul class="menu">
+            `;
+            
+        }
+        
+        el += `
+        <li class="container-img-info right">
+            <a href="./store_order.html?id=${params.id}&name=${menu.name}">
+                <img onerror="this.style.display='none'" src="${menu.url}" alt="${menu.category}의 ${menu.name}" />
+                <ul class="menu-info">
+                <li class="menu-info-name"><h3>${menu.name}</h3></li>
+                <li class="menu-info-detail">${menu.content}</li>
+                <li class="menu-info-price price">${menu.price}</li>
+                </ul>
+            </a>
+        </li>
+        `;
+        menus_list.innerHTML = el;
+    });
+};
 
 (()=>{
 
     setDetail();
 
+    setMenus();
+
+    const prices = document.querySelectorAll(".price");
+    convertPrices(prices);
+
+    const menu_btns = document.querySelectorAll(".menu-btn");
     for(let btn of menu_btns){
 
         btn.addEventListener("click",(e)=>{
@@ -45,5 +82,6 @@ const setDetail = () => {
         }); 
         
     }
-                                                                                                                                                                                       
+             
+
 })();
