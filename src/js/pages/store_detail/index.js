acd03b87c5faec1bf0e61ab8695d50d2fdcc6679
PisoDev77 <pisodev0427@gmail.com>
2022-09-28 11:35:15 +0900

    아이디값 get 데이터로 주고받기 성공

--- src/js/pages/store_detail/index.js
+++ src/js/pages/store_detail/index.js
@@ -1,22 +1,9 @@
 import {toggleClassList} from "../../utils/ClassListHandler.js";
+import { getParam } from "../../utils/Param.js";
 
 const menu_btns = document.querySelectorAll(".menu-btn");
 const cart = document.querySelector(".st-cart");
 
-const getParam = (search) => {
-    if(search){
-        const tmp = search.substring(1).split("&");
-        const data = {};
-        tmp.map((i)=>{
-            const sp = i.split("=");
-            data[sp[0]] = sp[1];
-        });
-
-        return data;
-    }else{
-        return false;
-    }
-}
 (()=>{
     
     const params = getParam(location.search);
--- src/js/pages/store_list/index.js
+++ src/js/pages/store_list/index.js
@@ -1,10 +1,31 @@
+import { addClassList } from "../../utils/ClassListHandler.js";
+import {stores} from "../../mock/stores.json";
 
 const categorys = document.querySelector(".st-list-category");
 const scrolList = document.querySelector(".st-list-scroll");
 const sortbys = document.querySelector(".st-list-sortbys");
+const list = document.querySelector(".st-list-scroll");
+
+const makeList = (store) =>{
+    const res = document.createElement("li");
+    res.className ="scroll-item container-img-info left";
+    const anchor = document.createElement("a");
+    anchor.href = `/store_detail.html?id=${store.id}`;
+    anchor.innerHTML =`
+        <img src="${store.url}" alt="${store.alt}" />
+        <ul>
+            <li class="scroll-item-name">${store.title}</li>
+            <li class="scroll-item-menu"><i class="fa-solid fa-star-sharp"></i>5.0(100+) 대부 가래떡 떡볶이, 부산꼬지어묵, 치즈세트</li>
+            <li>최소주문 ${store.minprice}원, 배달팁 ${store.deliverytip}원</li>
+        </ul>
+        <div class="scroll-item-time" aria-label="소요 시간"><i class="fa-light fa-clock"></i>38~53분</div>
+    `;
+    res.appendChild(anchor);
+    console.log(res);
+    return res;
+}
 
 (()=>{
-    console.log("asdasd");
     categorys.addEventListener("wheel",(e)=>{
         const direction = e.deltaY;
         if(direction > 0){
@@ -57,5 +78,9 @@ const sortbys = document.querySelector(".st-list-sortbys");
         // }
  
      });
+
+     for(let i of stores){
+        list.appendChild(makeList(i));
+     }
 
 })();
