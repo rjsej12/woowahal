5d14eb87f46b3d864a9880058069a86b9865ed4d
rjsej12 <rjsej12@naver.com>
2022-09-29 20:52:45 +0900

    webpack.config.js 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,18 @@
-const path = require('path');
+const path = require("path");
 
-module.exports={
+module.exports = {
   entry: {
-    cart: './src/js/pages/cart',
-    category: './src/js/pages/category',
-    main: './src/js/pages/main',
-    result: './src/js/pages/result',
-    store_detail: './src/js/pages/store_detail',
-    store_list: './src/js/pages/store_list',
-    store_order: './src/js/pages/store_order',
+    cart: "./src/js/pages/cart",
+    category: "./src/js/pages/category",
+    main: "./src/js/pages/main",
+    result: "./src/js/pages/result",
+    store_detail: "./src/js/pages/store_detail",
+    store_list: "./src/js/pages/store_list",
+    store_order: "./src/js/pages/store_order",
   },
 
   output: {
-    path: path.resolve(__dirname, './public/js/'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "./public/js/"),
+    filename: "[name].js",
   },
-
-  watch: true,
-  mode: "development"
-}
+};
